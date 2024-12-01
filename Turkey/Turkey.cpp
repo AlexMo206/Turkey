4068b4cd946e2f9169c18ec5c4a8d0377fd05d0d
AlexMo206 <AlexM12806@gmail.com>
2024-12-01 11:52:54 -0500

    Creating the turkey

---
+++ Turkey/Turkey.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "       ____    " << endl;
+    cout << "     .'    '.  " << endl;
+    cout << "    /  o  o  \\ " << endl;
+    cout << "   :    --    : " << endl;
+    cout << "    \\ -V-   /  " << endl;
+    cout << "     '..__..'   " << endl;
+    cout << "      V || V     " << endl;
+    cout << "     _V_||_V_   " << endl;
+    cout << "    /   ||   \\ " << endl;
+    cout << "   /    ||    \\ " << endl;
+    cout << "  /  V  ||  V  \\ " << endl;
+    cout << " (      ||      )" << endl;
+    cout << "  \\  v ||  v  / " << endl;
+    cout << "   \\_________/   " << endl;
+    cout << "     \\_____/     " << endl;
+    cout << "      | || |      " << endl;
+    cout << "      / // /      " << endl;
+
+    return 0;
+}
